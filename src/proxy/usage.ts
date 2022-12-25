19666f3163d9e3b1844357f987d95276cb140593
seal-tm <tobias.mulzer@sealsystems.de>
2022-12-25 20:03:18 +0100

    .

--- src/proxy/usage.ts
+++ src/proxy/usage.ts
@@ -1,12 +1,9 @@
 import { GenericProxy } from "./types";
 
+//geht nicht... proxies für interne Object schlagen häufig fehl
 const x = GenericProxy.of(new Map<string, string>(), {
   get(target, prop) {
     console.log(`I am a proxy getter: ${String(prop)}`);
     return target.get(String(prop));
-  },
-
-  apply(target, thisArg, argArray) {
-    target.apply(thisArg, argArray);
   },
 });
